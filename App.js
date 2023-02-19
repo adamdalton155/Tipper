493a9773c2d46cadde964a9ba27589ff90b0623e
adamdalton155 <adamdalton2016@gmail.com>
2023-02-19 22:14:18 +0000

    Started sign in/up screen

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import SignInScreen from './src/screens/SignInScreen';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+} from 'react-native';
+  
+function App() {
+
+  return (
+    <SafeAreaView style={styles.root}>
+      <SignInScreen></SignInScreen>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+ root:{
+  flex: 1
+ }
+});
+
+export default App;
